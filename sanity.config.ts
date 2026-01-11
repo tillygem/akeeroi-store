a18e7a5036a635efbd4d9a04d93d517ed4694157
TillyGem <oggemceo@gmail.com>
2026-01-11 09:27:37 +0000

    Fix admin page imports

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,17 @@
+"use client";
+
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import { schemaTypes } from '@/sanity/schemaTypes'
+import { schemaTypes } from '@/sanity/schemaTypes' 
 
 export default defineConfig({
   name: 'default',
-  title: 'Akee-Roi Collections',
+  title: 'Akee-Roi Admin',
 
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   
-  basePath: '/admin', 
+  basePath: '/admin',
 
   plugins: [structureTool()],
 
